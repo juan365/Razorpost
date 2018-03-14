3860effcfd5ddbf6c7e0dfcc99040663f336537e
juan365 <35336764+juan365@users.noreply.github.com>
2018-03-14 10:14:08 +0100

    a

---
+++ Razorpost/Controllers/PlantillaController.cs
@@ -0,0 +1,46 @@
+﻿using EjemploRazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace EjemploRazor.Controllers
+{
+    public class PlantillaController : Controller
+    {
+        ModeloPlantilla modelo;
+        // GET: Plantilla
+        public PlantillaController()
+        {
+            this.modelo = new ModeloPlantilla();
+        }
+
+        public ActionResult Ejemplo1()
+        {
+            ViewBag.Mensaje = "Hola mundo";
+            return View();
+        }
+        public ActionResult Ejemplo2()
+        {
+            MADRID jugador = modelo.GetJugador();
+            return View(jugador);
+        }
+        public ActionResult Ejemplo3()
+        {
+            List<MADRID> lista = modelo.GetPlantilla();
+            return View(lista);
+        }
+        public ActionResult Plantilla()
+        {
+            List<MADRID> lista = modelo.GetPlantilla();
+            return View(lista);
+        }
+        public ActionResult Details(int dorsal)
+        {
+            MADRID jugador = modelo.GetDetalles(dorsal);
+            return View(jugador);
+        }
+    }
+}
---
+++ Razorpost/Models/ModeloPlantilla.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EjemploRazor.Models
+{
+    public class ModeloPlantilla
+    {
+        REAL_MADRIDEntities contexto;
+        public ModeloPlantilla()
+        {
+            this.contexto = new REAL_MADRIDEntities();
+        }
+        public MADRID GetJugador()
+        {
+            var consulta = from datos in contexto.MADRID
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+        public List<MADRID> GetPlantilla()
+        {
+            var consulta = from datos in contexto.MADRID
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public MADRID GetDetalles(int dorsal)
+        {
+            var consulta = from datos in contexto.MADRID
+                           where datos.DORSAL == dorsal
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+    }
+}
